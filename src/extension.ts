35b636a358c04f1d323ed70e6db264dbfaaf979a
kevilin.rodrigues <kevilin.rodrigues@king.local@CW001994.king.local>
2023-11-08 00:27:12 -0300

    Adicionando evento após selecionar texto

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,28 @@
 import * as vscode from 'vscode';
-import * as fs from 'fs';
 import axios from 'axios';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "security-verificator" is now active!');
-
-    let disposable = vscode.commands.registerCommand('security-verificator.checarSeguranca', async () => {
+    let disposable = vscode.commands.registerCommand('extension.verificarVulnerabilidades', async () => {
         const editor = vscode.window.activeTextEditor;
         if (editor) {
-            const document = editor.document;
-            const text = document.getText();
+            const { selection, document } = editor;
+            const selectedText = document.getText(selection);
 
-            try {
-                // Fazendo uma requisição POST para a API
-                const response = await axios.post('https://chatgpt-api.kmr.dev.br/api/check', {
-					origem: "teste",
-					entrada: text
-                });
-
-                const mensagem = response.data.retorno;
-                
-                vscode.window.showInformationMessage(mensagem);
-            } catch (error: any) {
-                vscode.window.showErrorMessage('Erro ao fazer a requisição para a API: ' + error.message);
+            if (selectedText) {
+                try {
+                    const response = await axios.post('https://chatgpt-api.kmr.dev.br/api/check', {
+                        origem: "vscode",
+                        entrada: selectedText
+                    });
+    
+                    const mensagem = response.data.retorno;
+                    
+                    vscode.window.showInformationMessage(mensagem);
+                } catch (error: any) {
+                    vscode.window.showErrorMessage('Erro ao fazer a requisição para a API: ' + error.message);
+                }
+            } else {
+                vscode.window.showWarningMessage('Nenhum texto selecionado.');
             }
         } else {
             vscode.window.showErrorMessage('Nenhum editor aberto para obter o conteúdo do arquivo.');
